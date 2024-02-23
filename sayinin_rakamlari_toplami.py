79f6d2f4c31fe366a5f8037481b9d2b3e0ced469
mahmutozsoy21 <127337869+mahmutozsoy21@users.noreply.github.com>
2024-02-23 20:42:57 +0300

    Update sayinin_rakamlari_toplami.py

--- sayinin_rakamlari_toplami.py
+++ sayinin_rakamlari_toplami.py
@@ -1,9 +1,14 @@
 # ekrandan alinan bir sayinin rakamlari toplamini bulan program yazınız
 
-sayi=str(input("lütfen sayi giriniz:"))
+sayi=str(input("lütfen sayi giriniz:"))# sayinin rakamlarini indexlemek için str yaptık
+
+
 rakam_toplami=0
+
 for i in sayi:
-    rakam_toplami+=int(i)
+    
+    rakam_toplami+=int(i)# burda str ifadesini int değer ile toplamak için int yaptık
+    
     
 print(rakam_toplami)
     
