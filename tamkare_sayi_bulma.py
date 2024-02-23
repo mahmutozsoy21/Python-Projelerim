d5262ab3bdabc3f67824e0c8e75616dc8acaa30e
mahmutozsoy21 <127337869+mahmutozsoy21@users.noreply.github.com>
2024-02-23 21:17:15 +0300

    Add files via upload

---
+++ tamkare_sayi_bulma.py
@@ -0,0 +1,16 @@
+# ekrana girilen bir sayinin herhangi bir sayinin karesi olup olmadığını bulma
+
+sayi=int(input("sayi giriniz:"))
+
+if sayi**0.5==int(sayi**0.5):# sayinin karekökünün bir tam sayiya eşit olup olmadıını kontrol ettik
+
+    print(sayi,"tam kare sayidir")
+
+else:
+    print(sayi,"tam kare sayi değildir")
+
+
+
+
+
+
