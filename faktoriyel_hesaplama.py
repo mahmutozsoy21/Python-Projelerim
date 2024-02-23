ac57cad7de277b0963279cbef0211e8052747816
mahmutozsoy21 <127337869+mahmutozsoy21@users.noreply.github.com>
2024-02-23 20:19:34 +0300

    Add files via upload

---
+++ faktoriyel_hesaplama.py
@@ -0,0 +1,56 @@
+# ekrandan alınan bir sayının faktoriyelini hesaplama
+
+"""sayi=int(input("Bir sayi giriniz:"))
+faktoriyel=1
+
+for i in range(1,sayi+1):
+    faktoriyel*=i
+    
+    
+print(f"{sayi}!={faktoriyel}")"""
+
+
+sayi=int(input("sayi giriniz:"))
+faktoriyel=1
+i=1
+
+while i<=sayi:
+    faktoriyel*=i
+    i+=1
+print(f"{sayi}!={faktoriyel}")
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
