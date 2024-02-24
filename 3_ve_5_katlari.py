ee8f33a52b69d95bc80271721eacc6ec050d0ec3
mahmutozsoy21 <127337869+mahmutozsoy21@users.noreply.github.com>
2024-02-24 14:22:56 +0300

    Update 3_ve_5_katlari.py

--- 3_ve_5_katlari.py
+++ 3_ve_5_katlari.py
@@ -1,13 +1,15 @@
 # 1000 den küçük 3 veya 5 in katları olan sayıların toplamını yazdıran program
-x=0
 toplam=0
 
-for i in range(1000):
-    if i%3==0 or i%5==0:
-        toplam+=i
+
+for i in range(1000):# 0 dan 999 a kadar sayıları deniyor
+
+    if i%3==0 or i%5==0:# sayinin 3 veya 5 in katı olup olmadığını kontrol ediyor
+        
+        toplam+=i# eğer sonuçlar doğru ise i sayısını toplama ekliyor
 
 
-print(toplam)
+print("Sonuç:",toplam)
 
 
 
